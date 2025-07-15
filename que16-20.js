56282ac80ab75f21d5b9c015cd7750ed05f385a7
Purushotham-20 <puthatarak036@gmail.com>
2025-07-15 08:00:31 +0530

    Create que16-20.js

---
+++ que16-20.js
@@ -0,0 +1,30 @@
+// Question 16
+function sum(a,b)
+{
+    return a+b;
+}
+console.log(sum(5,3));
+// Question 17
+function sent(name,color)
+{
+    console.log(name+"'s favourite color is "+color);
+}
+sent("Nithin","Black");
+// Question 18
+function avg(n1,n2,n3)
+{
+    console.log("Average of "+n1+","+n2+","+n3+" is "+((n1+n2+n3)/3));
+}
+avg(12,13,2);
+// Question 19
+function rep(str,num)
+{
+    console.log(str.repeat(num));
+}
+rep("Nithin",3);
+// Question 20
+function leng(arr)
+{
+    return arr.length;
+}
+console.log(leng([1,2,3,4,5]));
