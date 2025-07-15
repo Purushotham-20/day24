5509ca23db370c3381b6e5882f3aedd0ba3a0eeb
Purushotham-20 <puthatarak036@gmail.com>
2025-07-15 07:59:17 +0530

    Create que1-5.js

---
+++ que1-5.js
@@ -0,0 +1,33 @@
+// Question 1
+function greet()
+{
+    console.log("Hello World");
+}
+greet();
+// Question 2
+function sayHello(name)
+{
+    console.log("Hello "+name);
+}
+sayHello("Nithin");
+// Question 3
+function square(num)
+{
+    console.log(num*num);
+}
+square(6);
+// Question 4
+function isEven(num)
+{
+    return num%2==0;
+}
+console.log(isEven(8));
+// Question 5
+function Check(age)
+{
+    if(age>=18)
+        return "Adult";
+    else
+        return "Minor";
+}
+console.log(Check(19));
